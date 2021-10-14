cfa290aa8650da4d66f246c1f99742f9387253ec
Khai Do <zaro0508@gmail.com>
2021-10-14 09:48:51 -0700

    add http stack configs

--- sceptre/itsandbox/templates/noop.py
+++ sceptre/itsandbox/templates/noop.py
@@ -2,6 +2,6 @@ from troposphere import Template
 from troposphere.cloudformation import WaitConditionHandle
 
 def sceptre_handler(scepter_user_data):
-    template = Template()
+    template = Template(Description=f"deploy {scepter_user_data['MyName']} template")
     template.add_resource(WaitConditionHandle("WaitConditionHandle"))
     return template.to_json()
